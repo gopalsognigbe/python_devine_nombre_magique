ed6bb794a56ea14a57f42f9599f3817076667c03
Gopal <gopalsognigbe@gmail.com>
2024-07-06 13:48:55 +0100

    Update main.py
    first commit

--- main.py
+++ main.py
@@ -1,54 +1,48 @@
-def afficher_information_personne(nom,age,taille=0):
-    print(f"votre nom est {nom}, vous avez {age} ans l'an prochain vous aurez {age + 1} ans")
-    if age == 1 or age ==2:
-        print('vous etes un bebe')
-    elif age < 10 :
-        print("vous etes mineur")
-    elif age == 17:
-        print('Vous etes presque majeur')
-    elif age >= 12 and age < 18:
-        print('vous etes adolescent')
-    elif age == 18:
-        print('vous etes tout juste majeur')
-    elif age > 60:
-        print('vous etes senior')
-    elif age > 18:
-        print('vous etes majeur')
-    # afficher la taille 
-    if taille != 0:
-        print('votre taille est :'+str(taille)+' m')
+import random
+
+NOMBRE_MIN=1
+NOMBRE_MAX=10
+NOMBRE_MAGIQUE=random.randint(NOMBRE_MIN,NOMBRE_MAX)
+vies=4
 
 
-def demander_nom ():
-    nom= input('entrer le nom: ')
-    while nom=='':
-        nom= input('entrer le nom: ')
-    return nom
-
-
-def demander_age(nom_personne):
-    age_int=0
-    while     age_int==0:
-        age=input(f'votre age {nom_personne} :')
+def demander_nombre(nb_min,nb_max):
+    a=0 
+    while a==0  :
+        nombre_str=input(f"Quel est le nombre magique(entre {nb_min} et{nb_max}) ? :")
         try:
-            age_int= int(age)
+            nombre_int=int(nombre_str)
         except:
-            print('Erreur:vous devez rentrer un nombre ')
-    return age_int
-""" 
-#demander nom
-nom=demander_nom()
-nom1=demander_nom()
+            print("Erreur: veuillez entrer un nombre svp")
+        else:
+            a=1
+            if nombre_int < nb_min or nombre_int > nb_max:
+                print(f'Respecter la limite !! le nombre doit etre entre {nb_min} et {nb_max}')
+                a=0
+    return nombre_int
 
-#demander age
-age=demander_age(nom)
-age1=demander_age(nom1)
 
-#afficher information
-afficher_information_personne(nom,age)1
-afficher_information_personne(nom1,age1)
-"""
-for i in range(0,3):
-    nom='personne '+ str(i +1)
-    age=demander_age(nom)
-    afficher_information_personne(nom,age,)
+def comparaison(nb,nb_magique,vie):
+    if nb > nb_magique:
+        print('le nombre magique est plus petit')
+        print(f'il vous reste {vie} vies')
+        
+    elif nb < nb_magique:
+        print('le nombre magique est plus grand ')
+        print(f'il vous reste {vie} vies')
+        
+    else :
+        print('Bravo vous avez trouvé le nombre magique !!!')
+
+
+nombre=demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
+comparaison(nombre,NOMBRE_MAGIQUE,vies)
+
+while nombre != NOMBRE_MAGIQUE and vies != 0 :
+    nombre=demander_nombre(NOMBRE_MIN,NOMBRE_MAX)
+    vies-=1
+    comparaison(nombre,NOMBRE_MAGIQUE,vies)
+    if vies == 0:
+        print(f'vous avez perdu !!! il vous reste {vies} vies')
+        print(f'le nombre magique etait {NOMBRE_MAGIQUE}')
+    
